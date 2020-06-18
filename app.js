7afd12e158dd6a925ce710314e44523376b151a9
Fahim Faisal <fsfahimsoikot12@gmail.com>
2020-06-18 20:51:46 +0600

    add animejs on nav bar

--- app.js
+++ app.js
@@ -14,14 +14,25 @@ function splitScroll() {
 
 splitScroll();
 
-header.onmousemove = function (e) {
+function replaceText(el) {
+    return el.innerHTML = el.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+}
+
+
+header.addEventListener('mousemove', function (e) {
+    let h1 = document.querySelector("#head-title");
     let moveX = e.offsetX / 15;
-    this.style.backgroundColor = `rgba(${moveX}, 125, 200,0.8)`;
-    let h1 = document.getElementById("head-title");
+    this.style.backgroundColor = `rgba(, 125, 200,0.8)`;
     let moveHeadX = e.offsetX / -20;
     let moveHeadY = e.offsetY / -35;
     h1.style.transform = `translate(${moveHeadX}px,${moveHeadY}px)`;
-};
+})
+
+let footer = document.querySelector('footer')
+footer.addEventListener('mousemove', function (e) {
+    let moveX = e.offsetX / 20;
+    this.style.backgroundColor = `rgba(${moveX}, 190, 195,0.8)`;
+})
 
 function parallax(element, transform, distance, speed) {
     const items = document.querySelectorAll(element)
@@ -39,17 +50,59 @@ window.onscroll = function () {
     letterSpace('h1', window.scrollY, 100)
 }
 
+
+
+let links = document.querySelectorAll('.list-item__link')
+links.forEach(link => {
+    replaceText(link)
+    link.addEventListener('click', function (e) {
+        const tl = anime.timeline({
+            loop: false
+        })
+        tl.add({
+                targets: this.children,
+                // scale: [1, 0.5],
+                translateX: [0, 20],
+                rotate: [0, 180],
+                opacity: [1, 0],
+                easing: 'easeInOutSine',
+                duration: 600,
+                delay: function (el, i) {
+                    return i * 100;
+                },
+            })
+            .add({
+                targets: this.children,
+                // scale: [0.5, 1],
+                translateX: [20, 0],
+                rotate: [180, 360],
+                opacity: [0, 1],
+                easing: 'easeInOutSine',
+                duration: 600,
+            })
+    })
+})
+
 let marker = document.querySelector('#marker')
 let items = document.querySelectorAll('.list-item__link')
 
 function indicator(e) {
     marker.style.left = e.offsetLeft + 'px'
     marker.style.width = e.offsetWidth + 'px'
+    console.log(e.offsetWidth)
 }
 items.forEach(item => {
-    item.onclick = function (e) {
-        items.forEach(link => link.classList.remove('act'));
-        this.classList.add('act')
-        indicator(e.target)
-    }
+    item.addEventListener('click', function (e) {
+        setTimeout(() => {
+            items.forEach(link => link.classList.remove('act'));
+            this.classList.add('act')
+            indicator(item)
+        }, 1200)
+    })
 })
+
+AOS.init({
+    offset: 400, // offset (in px) from the original trigger point
+    delay: 0, // values from 0 to 3000, with step 50ms
+    duration: 800 // values from 0 to 3000, with step 50ms
+});
